abdbd4dc2a2d714e2095263325534a2af467e136
rohankumar390 <87806043+rohankumar390@users.noreply.github.com>
2023-07-07 12:11:40 +0530

    Home updated

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import "./home.css";
-import myimage from "../images/myimage.jpg";
+import myimage from "../images/NewImg.png";
 const Home = () => {
   return (
-    <div className="home">
-      
-      <div className="home-img">
-        <img src={myimage} alt="profile pic" />
-        <h2>Akash</h2>
-        <h3>A Web Developer</h3>
-      </div>
-      {/* <div className="home-name">
-        
-      </div> */}
-      <div className="home-lines">
-        <h3>"Elevate your Online Presence with Innovative Web Development"</h3>
+    <div>
+      <div className="home">
+        <div className="home-txt">
+          <div>
+            <h1>I'm a creative web desinger</h1>
+            <div className="home-lines">
+              <h3>Elevate your Online Presence with Innovative Web Development</h3>
+            </div>
+            <div>
+              <a id="emailMe" href="mailto:rohanyt74@gmail.com">
+                <button className="button-34">
+                  Connect with me
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="home-img">
+          <img src={myimage} alt="profile pic" />
+        </div>
       </div>
     </div>
   );
 };
-
 export default Home;
